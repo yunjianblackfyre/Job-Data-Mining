102a02174b7f3f606ea1bceb3a01823aeca91547
Sibyl System <yunjianblackfyre@163.com>
2018-01-22 01:38:59 +0800

    Create interfaces.py

---
+++ job_data_mining/config/interfaces.py
@@ -0,0 +1,11 @@
+#   AUTHOR: Sibyl System
+#     DATE: 2018-01-01
+#     DESC: interfaces for programs
+
+DB_INTERFACE_CFG = {
+    'host':'localhost',
+    'port':3306,
+    'user':'root',
+    'password':'caonimabi',
+    'connection_timeout': 5000
+}
