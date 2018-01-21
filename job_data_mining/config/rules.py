4fa5bf38e31ee055b10610a0f70523ab16e588c9
Sibyl System <yunjianblackfyre@163.com>
2018-01-22 01:40:16 +0800

    Add files via upload

---
+++ job_data_mining/config/rules.py
@@ -0,0 +1,3 @@
+#   AUTHOR: Sibyl System
+#     DATE: 2018-01-01
+#     DESC: logic rules for programs
---
+++ job_data_mining/config/schedules.py
@@ -0,0 +1,3 @@
+#   AUTHOR: Sibyl System
+#     DATE: 2018-01-01
+#     DESC: schedules for programs
